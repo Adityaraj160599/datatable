48b8a833f5e7b680ae503a5c61fbcdfc66f68581
Adityaraj160599 <rathore.aditya@cubexo.io>
2022-06-21 18:59:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import MataTable from "./components/DataTable";
+
+function App()
+{
+  return(
+    <>
+    <div className='d-flex flex-coloum align-items-center'>
+      <h1>React Datatable</h1>
+      <MataTable/>
+    </div>
+    </>
+  )
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/DataTable.js
@@ -0,0 +1,78 @@
+import React,{useState,useEffect} from 'react';
+import axios from "axios"
+import DataTable from 'react-data-table-component'; 
+
+const MataTable=()=>{
+    const[data,setData]=useState([])
+    const[search,setSearch]=useState("")
+    const[filteredCountries,setFilteredCountries]=useState([])
+
+
+    const getData=async()=>{
+        try{
+            const response=await axios.get("https://restcountries.com/v2/all")
+            setData(response.data)
+            setFilteredCountries(response.data)
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+const coloums=[
+    {
+        name:'Country Name',
+        selector:(row)=>row.name,
+        sortable:true
+    },
+    {
+        name:'Country Native Name',
+        selector:(row)=>row.nativeName,
+    },
+    {
+        name:'Country Capital',
+        selector:(row)=>row.capital,
+    },
+    {
+        name:'Country Flag',
+        selector:(row)=><img height={50} width={50} src={row.flag}/>,
+    },
+    {
+        name:'Action',
+        cell:(row)=><button className='btn btn-primary'>Edit</button>
+    }
+        
+]
+useEffect (()=>{
+    getData()
+},[])
+
+useEffect(()=>{
+    const result = data.filter(country=>{
+        return country.name.toLowerCase().match(search.toLowerCase())
+    })
+
+    setFilteredCountries(result)
+},[search])
+
+return(
+    <DataTable
+    // title='Data Table'
+    columns={coloums}
+    data={filteredCountries}
+    pagination
+    fixedHeader
+    fixedHeaderScrollHeight="450px"
+    selectableRows
+    selectableRowsHighlight
+    subHeader
+    subHeaderComponent={
+        <input type="text" placeholder='Search Here' className='w-25 form-control'
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}/>
+        
+    }  
+    />
+)
+    }
+    export default MataTable
+
